a2542499ce697eeece8f96c38f0809ce416f43fb
midnight lunatic <arohkartikeya@gmail.com>
2026-05-26 20:01:45 +0530

    Minor change in MaxWealth

--- linear-search/MaxWealth.java
+++ linear-search/MaxWealth.java
@@ -3,18 +3,20 @@
 
 public class MaxWealth {
   public static void main(String[] args) {
-    maximumWealth(null)
+    int[][] arr = { { 1, 2, 3 }, { 24, 65, 3, 2, 5 }, { 7, 55, 122, 689, 4542, 1441 } };
+    System.out.println(maximumWealth(arr));
   }
+
   static int maximumWealth(int[][] accounts) {
-    int ans=Integer.MIN_VALUE;
-        for (int person=0;person< accounts.length;person++) {
-          int sum=0;
-          for (int account=0;account<accounts[person].length;account++){
-              sum+=accounts[person][account];
-          }
-          if (sum>ans)
-            ans=sum;
-        }    
-        return ans;
+    int ans = Integer.MIN_VALUE;
+    for (int person = 0; person < accounts.length; person++) {
+      int sum = 0;
+      for (int account = 0; account < accounts[person].length; account++) {
+        sum += accounts[person][account];
+      }
+      if (sum > ans)
+        ans = sum;
     }
+    return ans;
+  }
 }
